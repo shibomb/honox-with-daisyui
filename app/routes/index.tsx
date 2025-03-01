30487ea5e23405045d29e02e66b6cbbba2ea5d29
SHIBAHARA Hiroki <shibomb@gmail.com>
2025-03-02 05:18:55 +0900

    Update README and index route with GitHub link and minor text refinements

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -10,10 +10,21 @@ export default createRoute((c) => {
         <h1>Hello, {name}!</h1>
         <Counter />
       </div>
+      <hr />
       <div class="prose prose-base p-4">
         <h1>This is the simple template of HonoX with daisyUI v5.</h1>
+        <p>
+          Github:{" "}
+          <a
+            target="_blank"
+            href="https://github.com/shibomb/honox-with-daisyui"
+            rel="noopener noreferrer"
+          >
+            https://github.com/shibomb/honox-with-daisyui
+          </a>
+        </p>
         <ul>
-          <li>HonoX with tailwind CSS v4</li>
+          <li>HonoX with tailwindcss v4</li>
           <li>daisyUI v5</li>
           <li>Includes a Theme Selector inspired by the daisyUI website</li>
           <li>Includes a tailwind Typography demo page from daisyUI docs</li>
